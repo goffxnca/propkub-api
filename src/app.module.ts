83115aeb0a55c8bed20ff79233e0000efc92416d
Goff Patt <goffxnca@gmail.com>
2025-08-01 19:14:41 +0700

    Add log for post views cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,13 @@ import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostActionsModule } from './postActions/postActions.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+    ScheduleModule.forRoot(),
     UsersModule,
     AuthModule,
     ProvincesModule,
